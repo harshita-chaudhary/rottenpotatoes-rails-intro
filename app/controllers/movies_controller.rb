05c29a93cc29b93c0db7963d65bced73bb6f45b9
harshita-chaudhary <ec2-user@ip-172-31-16-95.us-east-2.compute.internal>
2019-10-04 00:40:22 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,17 +11,47 @@ class MoviesController < ApplicationController
   end
 
   def index
-
-    @sort_by = params[:sort_field]
-    @movies = Movie.order(@sort_by)
+    
+    setState = false
     
     @all_ratings = Movie.get_all_ratings
-    @set_ratings = params[:ratings]
-    puts(params['ratings'])
-    if params[:ratings]
-      @movies = Movie.where(:rating=>params[:ratings].keys).order(@sort_by)
+    
+    @sort_by = params[:sort_field]
+    
+    if @sort_by
+      session[:sort_field] = @sort_by
+    elsif session[:sort_field]
+      @sort_by=session[:sort_field]
+      setState = true
+    end
+    
+    if params[:commit] =='Refresh' and params[:ratings].nil?
+      @set_ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @set_ratings = params[:ratings]
+      session[:ratings] = @set_ratings
+    elsif session[:ratings]
+      @set_ratings = session[:ratings]
+      setState = true
     else
-      @movies = Movie.order(@sort_by)
+      @set_ratings = nil
+    end
+    
+    if setState
+      flash.keep
+      puts(@set_ratings)
+      redirect_to(:action=>'index',:sort_field=>@sort_by,:ratings=>@set_ratings)
+    end
+    
+    if @set_ratings and @sort_by
+      @movies = Movie.where(:rating=>@set_ratings.keys).order(@sort_by)
+    elsif @set_ratings
+      @movies = Movie.where(:rating=>params[:ratings].keys)
+    elsif @sort_by
+      @movies = Movie.all.order(params[:sort_field])
+    else
+      @movies = Movie.all
     end
     if !@set_ratings
       @set_ratings = Hash.new(@all_ratings)
